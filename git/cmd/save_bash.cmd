5510e8fb326e28394021de322157daca74a61524
buyn <buyn.tex@gmail.com>
2020-02-15 08:41:04 +0200

    new rl game on refactoring old

---
+++ git/cmd/save_bash.cmd
@@ -0,0 +1,2 @@
+copy /Y C:\Users\BuYn\.bashrc D:\Development\version-control.APP\Git\save\.bashre.bac
+copy /Y C:\Users\BuYn\.gitconfig D:\Development\version-control.APP\Git\save\.gitconfig.bac
---
+++ git/cmd/save_bash_cp.cmd
@@ -0,0 +1,2 @@
+copy /Y C:\Users\BuYn\.bashrc D:\Development\version-control.APP\Git\save\.bashre
+copy /Y C:\Users\BuYn\.gitconfig D:\Development\version-control.APP\Git\save\.gitconfig
